a925bb8ac2469b031a03a6229032194707fbb2ee
Thiago Vieira <thiiagovieir@gmail.com>
2020-06-02 20:14:29 -0300

    add - adicionando banco de dados

--- aulas/server/src/serve.ts
+++ aulas/server/src/serve.ts
@@ -1,46 +1,10 @@
 import express, { request, response } from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-//Rota: Endereço completo da requisição
-//Recurso: Qual entidade estamos acessando
-
-const users = [
-    'Thiago',
-    'Diego',
-    'Cleiton',
-    'Robson'
-];
-
-app.get('/users', (request, response) => {
-    const search =  String(request.query.search);
-    
-    const filteredUsers = search ? users.filter(users => users.includes(search)) : users;
-    
-    return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-    return response.json(users[id]);
-});
-
-app.post('/users', (request, response) => {
-
-    const data = request.body;
-
-    const user = {
-        name : data.name,
-        email : data.email
-    };
-
-    console.log(data);  
-
-    return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
-
 // npm run dev
