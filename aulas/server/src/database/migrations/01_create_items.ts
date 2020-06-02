cb26c4576a11c504c14f877a7c6170a13ea29d61
Thiago Vieira <thiiagovieir@gmail.com>
2020-06-02 20:52:36 -0300

    add 001 - create migrations

--- aulas/server/src/database/migrations/01_create_items.ts
+++ aulas/server/src/database/migrations/01_create_items.ts
@@ -1,15 +1,13 @@
 import Knex from 'knex';
+
 export async function up(knex: Knex){
-    knex.schema.createTable('points', table => {
+    return knex.schema.createTable('items', table => {
         table.increments('id').primary();
         table.string('image').notNullable();
-        table.string('name').notNullable();
-        table.string('email').notNullable();
-        table.string('whatsapp').notNullable();
-        table.decimal('latitude').notNullable();
-        table.decimal('longitude').notNullable();
-        table.string('city').notNullable();
-        table.string('uf', 2).notNullable();
+        table.string('title').notNullable();
     });
 }
-export async function down(){}
+
+export async function down(knex: Knex){
+    return knex.schema.dropTable('items');
+}
--- aulas/server/src/database/migrations/02_create_poit_items.ts
+++ aulas/server/src/database/migrations/02_create_poit_items.ts
@@ -1,15 +1,21 @@
 import Knex from 'knex';
+
 export async function up(knex: Knex){
-    knex.schema.createTable('points', table => {
+    return knex.schema.createTable('point_items', table => {
         table.increments('id').primary();
-        table.string('image').notNullable();
-        table.string('name').notNullable();
-        table.string('email').notNullable();
-        table.string('whatsapp').notNullable();
-        table.decimal('latitude').notNullable();
-        table.decimal('longitude').notNullable();
-        table.string('city').notNullable();
-        table.string('uf', 2).notNullable();
+        
+        table.integer('point_id')
+            .notNullable()
+            .references('id')
+            .inTable('points');
+        
+        table.integer('item_id')
+            .notNullable()
+            .references('id')
+            .inTable('items');
     });
 }
-export async function down(){}
+
+export async function down(knex: Knex){
+    return knex.schema.dropTable('point_items');
+}
