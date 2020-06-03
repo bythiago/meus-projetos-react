e3a2ddf46347da122967c0700392399bcaa2754e
Thiago Vieira <thiiagovieir@gmail.com>
2020-06-02 21:42:15 -0300

    add - pointItens

--- aulas/server/src/routes.ts
+++ aulas/server/src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import knex from './database/connection';
 
 const routes = express.Router();
@@ -8,12 +8,48 @@ routes.get('/items', async (request, response) => {
 
     const serializedItems = items.map(item => {
         return {
+            id : item.id,
             title : item.title,
             url : `http://localhost:3333/uploads/${item.image}`,
         };
     });
 
     return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const ids = await knex('points').insert({
+        image : 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    const pointItens = items.map((itemId: number) => {
+        return {
+            itemId,
+            pointId: ids[0],
+        }
+    });
+
+    await knex('point_items').insert(pointItens);
+
+    return response.json({ success : true });
 });
 
 export default routes;
