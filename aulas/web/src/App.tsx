2fcd6ae43606150eae965c45d27cb4ed76c08939
Thiago Vieira <thiiagovieir@gmail.com>
2020-06-03 20:32:00 -0300

    fix - correcoes na branch

---
+++ aulas/web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+      <Header title="Hello World"/>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque numquam, voluptate, sequi reiciendis explicabo molestiae aliquid nostrum dolorum nisi quaerat, minima optio dolores ipsum. Voluptatibus sequi reiciendis delectus eum excepturi.</h2>
+    </div>
+  );
+}
+
+export default App;
