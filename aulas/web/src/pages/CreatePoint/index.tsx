18bfbc986b965cc81a9ef7a64a461fc532f6f0e0
Thiago Vieira <thiiagovieir@gmail.com>
2020-06-03 22:06:56 -0300

    npm install axios

--- aulas/web/src/pages/CreatePoint/index.tsx
+++ aulas/web/src/pages/CreatePoint/index.tsx
@@ -64,6 +64,8 @@ const CreatPoint = () => {
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
+
+                        <Marker position={[-22.5736964,-44.1631088]}></Marker>
                     </Map>
 
                     <div className="field-group">
